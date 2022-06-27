e9d3a0bc286591dcff53c1469fce150939896285
Maurice Voors <m.voors@student.rug.nl>
2022-06-27 17:52:13 +0200

    Removed files from previous game version

--- final/open_json.php
+++
@@ -1,6 +0,0 @@
-<?php
-$json_file = file_get_contents("../data/game_data.json");
-$game_data = json_decode($json_file, true);
-$game_data = array_reverse($game_data);
-$last_move = $game_data[0];
-?>
